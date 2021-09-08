1f44ab510fa818adbbaeca42aeae39a8f0b0edb0
connectshark <bobhus394@gmail.com>
2021-09-08 10:22:12 +0800

    新增pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 /**
  * @type {import('vite').UserConfig}
  */
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({})],
   server: {
     port: 8080
   }
