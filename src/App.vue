59c05f912831a58e37d9d1eed77e7c77062b42a8
connectshark <bobhus394@gmail.com>
2021-09-08 17:49:11 +0800

    修正滿版bug

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,38 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted } from 'vue'
 import Head from './components/head.vue'
 export default {
   components: {
     Head
   },
   setup () {
-    return {
+    const resize = () => {
+      document.querySelector(':root').style.setProperty(`--vh`, window.innerHeight + 'px')
     }
+    onMounted(() => {
+      window.addEventListener('resize', resize)
+      resize()
+    })
+
+    onUnmounted(() => {
+      window.removeEventListener('resize', resize)
+    })
   }
 }
 </script>
 
 
 <style lang="scss">
+:root{
+  --vh: 100vh;
+}
+
 #app {
   font-family: 'openhuninn', 'Noto Sans TC', sans-serif;
   text-align: center;
 	background-color: rgba(209, 250, 229, .2);
-  min-height: 100vh;
+  height: var(--vh);
 }
 </style>
